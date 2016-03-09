a7265b8dd6b73b963d3e6f9d9d0037fb94e4a9ef
Irene LE SQUER <E124293B@S046V7pc16.ensinfo.sciences.univ-nantes.prive>
2016-03-09 14:14:14 +0100

    ajout de la classe Graphe et debut d'implémentation du menu utilisateur

---
+++ src/Graphe.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Graphe {
+	private int n;// nb de sommet
+	private int m = 0; // nb d'arete
+	private int degreMax = 0;// degre max
+	private int degreMoyen; // degre moyen
+	// liste de sucesseur representant le graphe
+	private ArrayList<ArrayList<Integer>> listeSuccesseur;
+
+	public Graphe(int n, double p) {
+		// nombre aleatoire pour generer les aretes
+		Random random = new Random();
+		// initialisation des valeurs
+		this.n = n;
+		this.listeSuccesseur = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < n; i++) {
+			this.listeSuccesseur.add(new ArrayList<Integer>());
+		}
+
+		// on construit les arretes
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				// si on est dans la proba
+				if (random.nextDouble() <= p) {
+					// on ajoute l'arete dans la liste de succ
+					this.listeSuccesseur.get(i).add(j);
+					this.listeSuccesseur.get(j).add(i);
+					// on en compte une de plus
+					this.m = this.m + 1;
+				}
+			}
+		}
+
+		// une fois le graphe construit on peut compter son degre.
+		int sommeDegres = 0;
+		for (ArrayList<Integer> listeSommet : listeSuccesseur) {
+			this.degreMax = Integer.max(this.degreMax, listeSommet.size());
+			sommeDegres += listeSommet.size();
+		}
+
+		this.degreMoyen = sommeDegres / n;
+	}
+
+	public String toString() {
+		return "nombre de sommet : " + n + "\nnombre d'arrete : " + m
+				+ "\ndegre max : " + degreMax + ", degre moyen : " + degreMoyen;
+		// + "\narete : " + listeSuccesseur.toString();
+	}
+	
+	public int getM() {
+		return m;
+	}
+	
+	public int getDegreMax() {
+		return degreMax;
+	}
+	
+	public int getDegreMoyen() {
+		return degreMoyen;
+	}
+	
+	public ArrayList<ArrayList<Integer>> getListeSuccesseur() {
+		return listeSuccesseur;
+	}
+	
+	public int getN() {
+		return n;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double p;
+		Graphe g;
+		System.out.println("Bonjour,");
+		Scanner sc = new Scanner(System.in);
+		int choix = Integer.MAX_VALUE;
+		int n;
+		while (choix != 0) {
+			System.out
+					.print(" quelle valeur de n (nombre de sommets) pour votre graphe ?\n");
+			n = sc.nextInt();
+			System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+			System.out.println("\t\t||\t\t\t\t\t\t||\t\t\tKERNEL-VC\t\t||\t\t\tARB-VC\t\t\t||\tGREEDY-VC\t\t||\tIPL-VC\t\t\t||");
+			System.out.println("|\tn="+n+"\t||\tm\t|\t\u0394\t|\tdM\t||\tTemps\t|\tVal\t|\tNb Exec\t||\tTemps\t|\tVal\t|\tNb Exec\t||\tTemps\t|\tVal\t||\tTemps\t|\tVal\t||");
+			System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+			p = ((double) n)/3;
+			g = new Graphe(n, p);
+			System.out.println("|p= n/3 = "+(double)(Math.floor(p*1000)/1000)+"\t||"+g.getM()+"\t\t|"+g.getDegreMax()+"\t\t|"+g.getDegreMoyen()+"\t\t||");
+			System.out.println("_________________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+
+			System.out.println("quitter : 0, tester une autre valeur de n : 1");
+			choix = sc.nextInt();
+			System.out.println("\n\n");
+			
+		}
+		sc.close();
+	}
+}
